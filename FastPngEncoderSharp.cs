69630cae29eecd30a128282817c92653a4106078
⭐️NINIKA⭐️ <dcnick3@users.noreply.github.com>
2025-02-16 22:15:38 +0300

    Update to newer version of ImageSharp, stop requiring contiguous memory

--- FastPngEncoderSharp.cs
+++ FastPngEncoderSharp.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Buffers;
 using System.IO;
 using System.Runtime.InteropServices;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
@@ -28,7 +30,7 @@ namespace FastPngEncoderSharp
             ColorType colorType;
             uint bitWidth;
             
-            if (!new Rectangle(Point.Empty, image.Size()).Contains(region))
+            if (!new Rectangle(Point.Empty, image.Size).Contains(region))
                 throw new ArgumentException("Region should be smaller than the source bounds.", nameof(region));
 
             T test = default;
@@ -56,21 +58,48 @@ namespace FastPngEncoderSharp
             
             sbyte* errorMessage = null;
             int result;
-            if (!image.TryGetSinglePixelSpan(out var singlePixelSpan))
-                throw new NotSupportedException(
-                    "To use the image with FastPngEncoderSharp the backing buffer must be continuous");
-            
-            fixed (byte* data = MemoryMarshal.Cast<T, byte>(singlePixelSpan))
+
+            // pin the image in memory for the duration of the native call
+            // the image is represented by an image group, so just pin each memory block independently
+            var memoryGroup = image.GetPixelMemoryGroup();
+            var memoryGroupHandles = new MemoryHandle[memoryGroup.Count];
             {
-                var rows = stackalloc byte*[region.Height];
+                var i = 0;
+                foreach (var memory in memoryGroup)
+                {
+                    memoryGroupHandles[i++] = memory.Pin();
+                }
+            }
+
+            try
+            {
+                // prepare an array of row data pointers for libpng
+                var rowPointers = stackalloc byte*[region.Height];
 
                 for (var j = 0; j < region.Height; j++)
-                    rows[j] = data + sizeof(T) * (region.X + (region.Y + j) * image.Width);
-
+                {
+                    var rowMemory = image.DangerousGetPixelRowMemory(j + region.Y);
+                
+                    // "pin" the span once again because AFAIK there's no way to just get a pointer :/
+                    // it's fine that the pointer escapes the `fixed` block, all image memory is already pinned above
+                    fixed (T* pointer = rowMemory.Span)
+                    {
+                        rowPointers[j] = (byte*)pointer + region.X * sizeof(T);
+                    }
+                }
+                
                 result = PngInteropHelper.write_png_to_file(filename, 
-                    rows, (uint) region.Width, (uint) region.Height, bitWidth,
+                    rowPointers, (uint) region.Width, (uint) region.Height, bitWidth,
                     colorType, InterlaceType.None, CompressionType.Default, FilterType.Default, TransformType.Identity, 
                     (uint)(image.Width * sizeof(T)), &errorMessage);
+            }
+            finally
+            {
+                // make sure the memory gets unpinned
+                for (var i = 0; i < memoryGroupHandles.Length; i++)
+                {
+                    memoryGroupHandles[i].Dispose();
+                }
             }
 
             var errorMessageString = errorMessage == null ? null : new string(errorMessage);
@@ -99,7 +128,7 @@ namespace FastPngEncoderSharp
                 WritePngToFileNative(filename, image, region);
             else
             {
-                if (image.Bounds() != region) 
+                if (image.Bounds != region) 
                     image.Mutate(o => o.Crop(region));
                 using (var fileStream = File.Create(filename))
                     image.SaveAsPng(fileStream);
